5b1a1f4071c40097755e6a083117a57399c6784d
Abdulsalam Ishaq <52103163+kayode-suc@users.noreply.github.com>
2020-07-18 15:53:16 -0700

    Initial Commit
    Initial Commit

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 $text = 'Simple #hashtag with #php';
 
-// find hashtags from text if any with regEx
-preg_match_all('/#(\w+)/', $text);
-
 // Replace hashtags with links
 $text = preg_replace('/#(\w+)/', '<a href="http://localhost/hashtag/$1">#$1</a>', $text);
 
